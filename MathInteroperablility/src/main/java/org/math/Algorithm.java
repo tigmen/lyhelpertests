7d503b7f9d3b5e17be935dd053097aee9216a06f
KarpoCod <2oivit160807@gmail.com>
2023-01-26 22:26:30 +0300

    добавили метод гауса (чекни, должно работать)

--- MathInteroperablility/src/main/java/org/math/Algorithm.java
+++ MathInteroperablility/src/main/java/org/math/Algorithm.java
@@ -31,7 +31,48 @@ public class Algorithm {
             return result;
         }
 
+        public List<Integer> GausMethod(Matrix inputmatrix, Integer[] intarg)
+        {
+            List<Integer> result = new ArrayList<>();
+            Integer[][] midMatrix = new Integer[inputmatrix.getMatrix().length][inputmatrix.getMatrix().length];
+            Integer[] midArg = new Integer[intarg.length];
+            Integer jx = 0;
+            //first
+            for(int j = 1; j < inputmatrix.getMatrix().length; j++){
+                if(j == 0){
+                    jx = 0;
+                    midArg[0] = intarg[0];
+                }
+                else{
+                    jx = j-1;
+                    midArg[j] = intarg[j]*inputmatrix.getMatrixElement(jx, j) - intarg[j]*inputmatrix.getMatrixElement(jx, j);
+                }
+                for(int i = 0; i < inputmatrix.getMatrix().length; i++) {
+                    if (j != 0){
+                        midMatrix[i][j] = inputmatrix.getMatrixElement(i, jx) * inputmatrix.getMatrixElement(jx, j) - inputmatrix.getMatrixElement(i, j) * inputmatrix.getMatrixElement(jx, jx);
+                    }
+                    else{
+                        midMatrix[i][0] = inputmatrix.getMatrixElement(i, 0);
+                    }
+                }
 
+            }
+
+            for (int i = inputmatrix.getMatrix().length - 1; i >= 0; i--)
+            {
+                if (i == inputmatrix.getMatrix().length - 1){
+                    continue;
+                }
+                else{
+                    for(int j = inputmatrix.getMatrix().length - 1; j > i; j--){
+                        midArg[i] -= result.get(j)*midMatrix[j][i];
+                    }
+                }
+                result.add(i, midMatrix[i][i]/midArg[i]);
+            }
+
+            return result;
+        }
 
     }
 
